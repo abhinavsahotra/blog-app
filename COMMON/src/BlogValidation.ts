cb91c06a24f50a4bd8e31a627caf1eba3a36711d
= <=>
2025-10-22 12:21:37 +0530

    Common

---
+++ COMMON/src/BlogValidation.ts
@@ -0,0 +1,16 @@
+import { z } from "zod"
+
+export const createBlogInput = z.object({
+    title: z.string(),
+    content: z.string(),
+})
+
+export type CreateBlogInput = z.infer<typeof createBlogInput>
+
+export const updateBlogInput = z.object({
+    title: z.string(),
+    content: z.string(),
+    id: z.number()
+})
+
+export type UpdateBlogInput = z.infer<typeof updateBlogInput>
