08ac7f84218444708e055e703fefee1d8df11412
= <=>
2025-10-23 11:52:25 +0530

    Deployment

--- FRONTEND/src/components/Appbar.tsx
+++ FRONTEND/src/components/Appbar.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import { Avatar } from "./BlogCard"
 
 export const Appbar = () => {
@@ -6,6 +7,9 @@ export const Appbar = () => {
             Medium
         </div>
         <div>
+            <Link to={`/publish`}>
+                <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 ">New</button>
+            </Link>
             <Avatar 
             size={"big"}
             name={"harkirat"} />
